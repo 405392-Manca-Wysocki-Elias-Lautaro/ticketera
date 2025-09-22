5bc8992122187bfef65a78368a1f334b8b7b4334
405392-Manca-Wysocki-Elias-Lautaro <405392@tecnicatura.frc.utn.edu.ar>
2025-09-21 21:23:57 -0300

    build: Configured docker on each microservice

--- backend/payment-service/src/main/java/com/payment/app/PaymentAppApplication.java
+++ backend/payment-service/src/main/java/com/payment/app/PaymentAppApplication.java
@@ -1,13 +1,13 @@
-package com.ticket.app.cmd;
+package com.payment.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketAppApplication {
+public class PaymentAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TicketAppApplication.class, args);
+		SpringApplication.run(PaymentAppApplication.class, args);
 	}
 
 }
--- backend/ticket-service/src/main/java/com/ticket/app/controllers/HealthController.java
+++ backend/ticket-service/src/main/java/com/ticket/app/controllers/HealthController.java
@@ -1,14 +1,12 @@
 package com.ticket.app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
 public class HealthController {
     
-    @GetMapping
+    @GetMapping("/health")
     public String health() {
         return "Ticket Service is running!";
     }
