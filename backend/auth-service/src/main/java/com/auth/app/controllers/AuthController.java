ba70a28102b7f7e4936af3507b9f1d22f4a9c625
405392-Manca-Wysocki-Elias-Lautaro <405392@tecnicatura.frc.utn.edu.ar>
2025-10-11 00:38:20 -0300

    feat(auth): add /verify endpoint to AuthController
    Added a new /verify endpoint in AuthController to handle user
    verification logic. This endpoint will be used to confirm email
    verification tokens and activate user accounts during the registration
    process.

--- backend/auth-service/src/main/java/com/auth/app/controllers/AuthController.java
+++ backend/auth-service/src/main/java/com/auth/app/controllers/AuthController.java
@@ -1,14 +1,16 @@
 package com.auth.app.controllers;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.auth.app.dto.request.RegisterRequest;
 import com.auth.app.dto.response.AuthResponse;
-import com.auth.app.services.AuthService;
+import com.auth.app.services.application.AuthService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,15 @@ public class AuthController {
 
         AuthResponse response = authService.register(request, userAgent, ipAddress);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<AuthResponse> verify(@RequestParam("token") String token, HttpServletRequest httpRequest) {
+        String userAgent = httpRequest.getHeader("User-Agent");
+        String ipAddress = httpRequest.getRemoteAddr();
+
+        authService.verifyEmail(token, userAgent, ipAddress);
+        return ResponseEntity.ok(null);
     }
 
 }
