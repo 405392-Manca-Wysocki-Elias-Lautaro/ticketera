2e93e00ff48547482485bc40f1c391d5f5009192
405392-Manca-Wysocki-Elias-Lautaro <405392@tecnicatura.frc.utn.edu.ar>
2025-10-31 21:06:09 -0300

    fix(cors): enable cookie forwarding and add global config
    Added cookie forwarding from backend to frontend and created
    CorsGlobalConfig in the gateway to centralize and improve CORS
    handling across all routes.

--- backend/auth-service/src/main/java/com/auth/app/configs/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/auth/app/configs/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.auth.app.configs;
 
-import java.beans.Customizer;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
